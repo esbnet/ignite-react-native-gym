e63a856c51c11303deddeab35434524148f84f80
esbnet <esbnet@gmail.com>
2023-11-29 17:34:47 -0300

    feat: :sparkles: created exerciseCard component

---
+++ src/components/ExcerciseCard.tsx
@@ -0,0 +1,32 @@
+import { Entypo } from "@expo/vector-icons";
+import { HStack, Heading, Icon, Image, Text, VStack, } from "native-base";
+import React from "react";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+type ExerciseCardProps = TouchableOpacityProps & {};
+
+export function ExcerciseCard({ ...props }: ExerciseCardProps) {
+  return (
+    <TouchableOpacity {...props}>
+      <HStack bg="gray.500" alignItems="center" p={2} rounded="md" mb={3}>
+        <Image
+          source={{ uri: "https://i.ibb.co/0jqHpnp/smartwatch.png" }}
+          alt="Imagem do exercício"
+          w={16}
+          h={16}
+          rounded="md"
+          mr={4}
+          resizeMode="center"
+        />
+        <VStack flex={1}>
+          <Heading color="white" fontSize="lg">
+            Remada unilateral
+          </Heading>
+          <Text color="gray.200" fontSize="sm" mt={1} numberOfLines={2}>
+            3 séries x 12 repetições
+          </Text>
+        </VStack>
+        <Icon as={Entypo} name="chevron-thin-right" color="gray.300" />
+      </HStack>
+    </TouchableOpacity>
+  );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,26 +1,53 @@
+import { ExcerciseCard } from "@/components/ExcerciseCard";
 import { Group } from "@/components/Group";
 import { HomeHeader } from "@/components/HomeHeader";
-import { HStack, VStack } from "native-base";
+import { FlatList, HStack, Heading, Text, VStack } from "native-base";
 import { useState } from "react";
 
 export function Home() {
   const [groupSelected, setGroupSelected] = useState("costa");
+  const [groups, setGroups] = useState([
+    { name: "costa", isMember: true },
+    { name: "ombro", isMember: true },
+    { name: "bíceps", isMember: true },
+    { name: "treíceps", isMember: true },
+    { name: "outros", isMember: true },
+  ]);
 
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack px={8} mt={8} justifyContent="space-between" alignItems="center">
-        <Group
-          name="ombro"
-          isActive={groupSelected === "ombro"}
-          onPress={() => setGroupSelected("ombro")}
-        />
-        <Group
-          name="costa"
-          isActive={groupSelected === "costa"}
-          onPress={() => setGroupSelected("costa")}
-        />
-      </HStack>
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => (
+          <Group
+            name={item.name}
+            isActive={groupSelected === item.name}
+            onPress={() => setGroupSelected(item.name)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 6 }}
+        my={8}
+        maxHeight={10}
+      />
+
+      <VStack flex={1} px={6}>
+        <HStack justifyContent={"space-between"} alignItems="center" mb={5}>
+          <Heading color="gray.200" fontSize="md" fontFamily="heading">
+            Exercícios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {groups.length}
+          </Text>
+        </HStack>
+
+        <ExcerciseCard />
+        <ExcerciseCard />
+      </VStack>
     </VStack>
   );
 }
